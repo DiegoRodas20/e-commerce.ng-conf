69bbb23662e2c285377c4fe61fd40a6fde9cc78e
DiegoRodas20 <diegorodas2008@hotmail.com>
2024-10-26 15:22:33 -0500

    Adding routes of bounded contexts

---
+++ src/app/shell/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-navbar',
+    standalone: true,
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css'
+})
+export class NavBarComponent implements OnInit {
+
+    ngOnInit() {
+        throw new Error('Method not implemented.');
+    }
+}
